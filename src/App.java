cd6cf8a93329ddfc81e0e5f71d3bbad56d791cdc
mateuskuritza <mateuskuritza@gmail.com>
2023-01-12 11:37:02 -0300

    repeat numbers table

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int maxNumber = 7;
+
+        for (int i = 1; i <= maxNumber; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
